841c416321a9e2cef5fd88094199a08bab27bc65
Srushti Summanwar <srushti@Srushtis-MacBook-Air.local>
2022-05-19 12:04:34 +0530

    updates

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.nutritionapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.nutritionapp to javafx.fxml;
+    exports com.example.nutritionapp;
+}
